560d6bb32f4571edafa6e9026a1b424d741c254d
Chengxin Guo <guochengxin2013@163.com>
2023-09-25 15:15:56 +0800

    new

--- src/kernel/level2/alphasparse_spmv_csr_merge_ginkgo.h
+++ src/kernel/level2/alphasparse_spmv_csr_merge_ginkgo.h
@@ -6,9 +6,6 @@
 #define SPMV_MERGE_BLOCK_SIZE 512
 #define BLOCK_SIZE 512
 
-// cudaEvent_t event_start2, event_stop2;
-// float elapsed_time2 = 0.0;
-
 template <typename T>
 __forceinline__ __device__ void merge_path_search(
     const T diagonal, const T x_len, const T y_len,
@@ -21,7 +18,7 @@ __forceinline__ __device__ void merge_path_search(
     while (x_min < x_max)
     {
         pivot = (x_max + x_min) / 2;
-        if (a[pivot / 2] < offset_y + diagonal - pivot)
+        if (a[pivot] < offset_y + diagonal - pivot)
         {
             x_min = pivot + 1;
         }
@@ -51,18 +48,17 @@ __global__ __launch_bounds__(SPMV_MERGE_BLOCK_SIZE) void merge_path_spmv(
 {
     extern __shared__ char buffer[];
 
-    T block_start_x2 = block_start_xs[blockIdx.x];
-    T block_start_x = block_start_x2 / 2;
+    const T block_start_x = block_start_xs[blockIdx.x];
     const T block_start_y = block_start_ys[blockIdx.x];
-    const T block_num_rows2 = block_start_xs[blockIdx.x + 1] - block_start_x2;
-    const T block_num_rows = block_num_rows2 / 2;
+    const T block_num_rows = block_start_xs[blockIdx.x + 1] - block_start_x;
     const T block_num_nnz = block_start_ys[blockIdx.x + 1] - block_start_y;
+
     T *shared_row_ptrs = reinterpret_cast<T *>(buffer);
     T *shared_col_idxs = reinterpret_cast<T *>(shared_row_ptrs + block_num_rows);
     U *shared_val = reinterpret_cast<U *>(shared_row_ptrs + SPMV_MERGE_BLOCK_SIZE * ITEMS_PER_THREAD);
 
     // 66511.4
-    for (int i = threadIdx.x; i <= block_num_rows; i += SPMV_MERGE_BLOCK_SIZE)
+    for (int i = threadIdx.x; i < block_num_rows; i += SPMV_MERGE_BLOCK_SIZE)
     {
         shared_row_ptrs[i] = row_end_ptrs[block_start_x + i];
     }
@@ -86,97 +82,53 @@ __global__ __launch_bounds__(SPMV_MERGE_BLOCK_SIZE) void merge_path_spmv(
     T start_y;
 
     // 266012.4
-    merge_path_search<T>(IPT * threadIdx.x, block_num_rows2,
+    merge_path_search<T>(IPT * threadIdx.x, block_num_rows,
                          block_num_nnz, shared_row_ptrs, block_start_y,
-                         &start_x2, &start_y);
-    // start_x2 = (start_x2 + 1) / 2 * 2;
+                         &start_x, &start_y);
+
     // 1291546.2
-    T row_i2 = block_start_x2 + start_x2;
     T ind = block_start_y + start_y;
+    T row_i = block_start_x + start_x;
     U value = U{};
     T temp_row_ptr = shared_row_ptrs[start_x];
 #pragma unroll
-    for (T i = 0; i < IPT && row_i < num_rows && start_y < block_num_nnz; i++)
+    for (T i = 0; i < IPT && row_i < num_rows; i++)
     {
-        // if (start_x >= block_num_rows)
+        // if (start_x == block_num_rows)
         // {
-        //     printf("start_x:%d, block_num_rows:%d, start_x2:%d, block_num_rows2:%d\n", start_x, block_num_rows, start_x2, block_num_rows2);
-        // }
-        // if (start_y >= block_num_nnz)
-        // {
-        //     printf("start_y:%d, block_num_nnz:%d\n", start_y, block_num_nnz);
+        //     printf("i:%d, tid:%d, bid:%d, y[row_i]:%f, value:%f, ind:%d, start_x:%d, start_y:%d, block_num_nnz:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
+        //            i, threadIdx.x, blockIdx.x, y[row_i], value, ind, start_x, start_y, block_num_nnz, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
         // }
         if (ind < temp_row_ptr || start_x == block_num_rows)
         {
 
-        // if (start_x2 % 2 == 1)
-        // {
-        //     start_x2++;
-        //     row_i2++;
-        //     continue;
-        // }
-
-        // if (blockIdx.x == 1)
-        //     printf("start_x:%d, start_y:%d, row_i:%d, i:%d, block_num_rows2:%d\n", start_x, start_y, row_i, i, block_num_rows2);
-        // if (row_i == 857)
-        // {
-        //     printf("i:%d, tid:%d, bid:%d, block_start_x:%d, block_start_y:%d, y[row_i]:%f, value:%f, block_num_rows2:%d, row_i2:%d, ind:%d, start_x2:%d, start_y:%d, block_num_nnz:%d, start_x:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
-        //            i, threadIdx.x, blockIdx.x, block_start_x, block_start_y, y[row_i], value, block_num_rows2, row_i2, ind, start_x2, start_y, block_num_nnz, start_x, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-        // }
-        // if (row_i == 857)
-        // {
-        //     printf("ind:%d, shared_row_ptrs[start_x]:%d\n",
-        //            ind, shared_row_ptrs[start_x]);
-        // }
-        if (ind < shared_row_ptrs[start_x])
-        {
             value += shared_val[start_y] * __ldg(&x[shared_col_idxs[start_y]]);
             start_y++;
             ind++;
         }
         else
         {
-            // if (row_i == 1)
-            // {
-            //     // printf("start_y:%d, \n",
-            //     //        start_y, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-            //     printf("tid:%d, y[row_i]:%f, value:%f, block_num_rows2:%d, row_i2:%d, ind:%d, start_x2:%d, start_y:%d, block_num_nnz:%d, start_x:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
-            //            threadIdx.x, y[row_i], value, block_num_rows2, row_i2, ind, start_x2, start_y, block_num_nnz, start_x, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-            // }./
-            atomicAdd(&y[row_i], alpha * value);
+            if (value != U{})
+            {
+                atomicAdd(&y[row_i], alpha * value);
+                value = U{};
+            }
             start_x++;
             row_i++;
             temp_row_ptr = shared_row_ptrs[start_x];
         }
-        // if (row_i == 2)
-        // {
-        //     printf("i:%d, tid:%d, bid:%d, y[row_i-1]:%f, value:%f, block_num_rows2:%d, row_i2:%d, ind:%d, start_x2:%d, start_y:%d, block_num_nnz:%d, start_x:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
-        //            i, threadIdx.x, blockIdx.x, y[row_i - 1], value, block_num_rows2, row_i2, ind, start_x2, start_y, block_num_nnz, start_x, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-        // }
-        // if (row_i == 1)
-        // {
-        //     printf("tid:%d, value:%f, block_num_rows2:%d, row_i2:%d, ind:%d, start_x2:%d, start_y:%d, block_num_nnz:%d, start_x:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
-        //            threadIdx.x, value, block_num_rows2, row_i2, ind, start_x2, start_y, block_num_nnz, start_x, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-        // }
     }
     // 140647.7
     const cooperative_groups::thread_block_tile<WARP_SIZE> tile_block =
         cooperative_groups::tiled_partition<WARP_SIZE>(g);
     bool head = segment_scan<WARP_SIZE>(tile_block, row_i, value);
-    // if (row_i == 5)
-    // {
-    //     // printf("start_y:%d, \n",
-    //     //        start_y, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-    //     printf("tid:%d, y[row_i]:%f, value:%f, block_num_rows2:%d, row_i2:%d, ind:%d, start_x2:%d, start_y:%d, block_num_nnz:%d, start_x:%d, row_i:%d, block_num_rows:%d, num_rows:%d, shared_val[start_y]:%f, shared_col_idxs[start_y]:%d, shared_row_ptrs[start_x]:%d\n",
-    //            threadIdx.x, y[row_i], value, block_num_rows2, row_i2, ind, start_x2, start_y, block_num_nnz, start_x, row_i, block_num_rows, num_rows, shared_val[start_y], shared_col_idxs[start_y], shared_row_ptrs[start_x]);
-    // }
     if (head)
     {
         atomicAdd(&y[row_i], alpha * value);
     }
 }
 
-template <typename T>
+template <typename T, typename V, typename W>
 __global__ __launch_bounds__(BLOCK_SIZE) void abstract_merge_path_search(
     T *block_start_xs,
     T *block_start_ys,
@@ -185,7 +137,9 @@ __global__ __launch_bounds__(BLOCK_SIZE) void abstract_merge_path_search(
     const T num_merge_items,
     const T items_per_block,
     const T block_num,
-    const T *row_end_ptrs)
+    const T *row_end_ptrs,
+    V *y,
+    W beta)
 {
     const T gid = blockIdx.x * blockDim.x + threadIdx.x;
     if (gid > block_num)
@@ -241,30 +195,23 @@ alphasparseStatus_t spmv_csr_merge_ginkgo(alphasparseHandle_t handle,
 
     T *block_start_xs = reinterpret_cast<T *>(externalBuffer);
     T *block_start_ys = reinterpret_cast<T *>(block_start_xs + block_num + 1);
-    const int maxbytes = items_per_block * (sizeof(U) + sizeof(T)) + sizeof(T);
-    printf("block_num:%d\n", block_num);
+    const int maxbytes = items_per_block * (sizeof(U) + sizeof(T));
+    // printf("block_num:%d\n", block_num);
     // printf("maxbytes:%d\n", maxbytes);
     // cudaFuncSetAttribute(abstract_merge_path_spmv<items_per_block, T, U, V, W>, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
     if (&alpha != nullptr && &beta != nullptr)
     {
-        abstract_merge_path_search<T><<<ceildivT(block_num, BLOCK_SIZE), BLOCK_SIZE>>>(
+        abstract_merge_path_search<T><<<ceildivT(block_num + m + 1, BLOCK_SIZE), BLOCK_SIZE>>>(
             block_start_xs,
             block_start_ys,
-            m * 2,
+            m,
             nnz,
             num_merge_items,
             items_per_block,
             block_num,
-            csr_row_ptr + 1);
-        // T *block_start_xs_h = (T *)malloc(sizeof(T) * (block_num + 1));
-        // T *block_start_ys_h = (T *)malloc(sizeof(T) * (block_num + 1));
-        // cudaMemcpy(block_start_xs_h, block_start_xs, sizeof(T) * (block_num + 1), cudaMemcpyDeviceToHost);
-        // cudaMemcpy(block_start_ys_h, block_start_ys, sizeof(T) * (block_num + 1), cudaMemcpyDeviceToHost);
-        // for (int i = 0; i < block_num + 1; i++)
-        // {
-        //     printf("x: %d, y: %d\n", block_start_xs_h[i] / 2, block_start_ys_h[i]);
-        // }
-        // GPU_TIMER_START(elapsed_time2, event_start2, event_stop2);
+            csr_row_ptr + 1,
+            y,
+            beta);
         if (block_num > 0)
         {
             merge_path_spmv<ITEMS_PER_THREAD><<<block_num, SPMV_MERGE_BLOCK_SIZE, maxbytes, 0>>>(
@@ -281,8 +228,6 @@ alphasparseStatus_t spmv_csr_merge_ginkgo(alphasparseHandle_t handle,
                 block_start_xs,
                 block_start_ys);
         }
-        // GPU_TIMER_END(elapsed_time2, event_start2, event_stop2);
-        // printf("compute_time1:%f ms\n", elapsed_time2);
     }
     else
     {
--- src/test/level2/spmv_csr_r_f32_test_metrics.cu
+++ src/test/level2/spmv_csr_r_f32_test_metrics.cu
@@ -45,7 +45,7 @@ const float beta = 3.2f;
 
 std::vector<double> cuda_time_list, alpha_time_list, cuda_bandwidth_list, alpha_bandwidth_list, cuda_gflops_list, alpha_gflops_list;
 std::vector<cusparseSpMVAlg_t> cu_alg_list = {CUSPARSE_SPMV_ALG_DEFAULT, CUSPARSE_SPMV_CSR_ALG1, CUSPARSE_SPMV_CSR_ALG2};
-std::vector<alphasparseSpMVAlg_t> alpha_alg_list = {ALPHA_SPARSE_SPMV_ALG_MERGE};
+std::vector<alphasparseSpMVAlg_t> alpha_alg_list = {ALPHA_SPARSE_SPMV_ROW_PARTITION};
 // std::vector<cusparseSpMVAlg_t> cu_alg_list = {CUSPARSE_SPMV_ALG_DEFAULT};
 // std::vector<alphasparseSpMVAlg_t> alpha_alg_list = {ALPHA_SPARSE_SPMV_ADAPTIVE};
 
